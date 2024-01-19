fd2cbffa47e250bcf635b351a52b86e02824a5da
GwangYoungHeo <heogwangyoung@gmail.com>
2024-01-20 02:47:18 +0900

    환경설정

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+
+            while (true)
+            {
+                #region 프레임관리
+                // FPS 프레임 (60 프레임 OK, 30프레임 이하로 NO)
+                int currentTick = System.Environment.TickCount;
+                
+                // 만약 경과한 시간이 1/30 보다 적다면 (currentTick 단위는 현재 ms임)
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+           }
+        }
+    }
+}
