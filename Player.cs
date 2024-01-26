646bfa9583498c5ca733f0b70ad7ec4b02b3297b
GwangYoungHeo <heogwangyoung@gmail.com>
2024-01-26 10:49:47 +0900

    맵생성

---
+++ Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Player
+    {
+        public int PosY { get; private set; }
+        public int PosX {  get; private set; }
+        Random _random = new Random();
+        Board? _board;
+
+        public void Initalize(int posY, int posX, int destY, int destX, Board board)
+        {
+            PosY = posY;
+            PosX = posX;
+
+            _board = board;
+        }
+
+        const int MOVE_TICK = 100;
+        int _sumTick = 0;
+        public void Update(int deltaTick)
+        {
+            _sumTick += deltaTick;
+            if (_sumTick >= MOVE_TICK)
+            {
+                _sumTick = 0;
+
+                // 0.1 초마다 실행될 로직
+                int randomValue = _random.Next(0, 5);
+                switch(randomValue)
+                {
+                    case 0: // 상
+                        if (_board!.Tile![PosY - 1, PosX] == Board.TileType.Empty)
+                            PosY -= 1;
+                        break;
+
+                    case 1: // 하
+                        if (_board!.Tile![PosY + 1, PosX] == Board.TileType.Empty)
+                            PosY += 1;
+                        break;
+
+                    case 2: // 좌
+                        if (_board!.Tile![PosY, PosX - 1] == Board.TileType.Empty)
+                            PosX -= 1;
+                        break;
+
+                    case 3: // 우
+                        if (_board!.Tile![PosY, PosX + 1] == Board.TileType.Empty)
+                            PosX += 1;
+                        break;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,16 +6,18 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            Board board = new Board();
-            board.Initalize(25);
 
-            //Console.SetWindowSize(25, 25);
+            Board board = new Board();
+            Player player = new Player();
+
+            board.Initalize(25, player);
+            player.Initalize(1, 1, board.Size - 2, board.Size - 2, board);
+
             Console.CursorVisible = false;
             Console.Clear();
 
             const int WAIT_TICK = 1000 / 30;
             
-
             int lastTick = 0;
 
             while (true)
@@ -27,12 +29,16 @@ namespace Algorithm
                 // 만약 경과한 시간이 1/30 보다 적다면 (currentTick 단위는 현재 ms임)
                 if (currentTick - lastTick < WAIT_TICK)
                     continue;
+
+                int deltaTick = currentTick - lastTick;
+
                 lastTick = currentTick;
                 #endregion
 
                 // 입력
 
                 // 로직
+                player.Update(deltaTick);
 
                 // 렌더링
                 Console.SetCursorPosition(0, 0);
